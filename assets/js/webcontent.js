bee66d97427ef8d1257f65a502a315d29e165aa2
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2023-11-13 04:14:04 +0000

    :tada:

--- assets/js/webcontent.js
+++ assets/js/webcontent.js
@@ -1,15 +1,16 @@
 let title = null;
 let description = null;
 let image = null;
+let parent = 'body'
 
 addEventListener("DOMContentLoaded", () => {
   if(!window.isBanter){
     const meta = document.getElementsByTagName('meta');
+    if(document.getElementById('banter-info')){parent = 'banter-info';}
     for (let i = 0; i < meta.length; i++) {
       let this_property = null;
       let this_content = null;
-      try{this_property = meta[i].attributes.property.value; this_content = meta[i].attributes.content.value;}
-      catch{console.log("couldn't read the value");}
+      try{this_property = meta[i].attributes.property.value; this_content = meta[i].attributes.content.value;}catch{}
       finally{switch(this_property){
         case 'og:title': title = this_content; break;
         case 'og:description': description = this_content; break;
@@ -28,10 +29,10 @@ function makeElement(type, html, link) {
   if(!type || !html){return;}
   let el = document.createElement(type);
   switch(type){
-    case 'img': el.src = html; el.setAttribute('width', '800px'); el.setAttribute('height', 'auto'); el.setAttribute('style', 'max-width:100%'); break;
+    case 'img': el.src = html; el.setAttribute('width', 'auto'); el.setAttribute('height', 'auto'); el.setAttribute('style', 'max-width:100%;max-height:420px;'); break;
     case 'a': if(link){el.href = link; el.innerHTML = html;} break;
     default: el.innerHTML = html;
   }
   if(type == 'a'){let p = document.createElement('p'); p.appendChild(el); document.querySelector('body').appendChild(p);}
-  else{document.querySelector('body').appendChild(el);}
+  else{document.querySelector(parent).appendChild(el);}
 }
