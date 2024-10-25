54d13f76a96140cd4144d7c4eb38b30ca02df43c
Rev. Ryan Fowler-Hughes <lunartiger@live.com>
2024-10-25 15:37:53 +0000

    :tada:

--- gravity-cube/gravity.js
+++ gravity-cube/gravity.js
@@ -1,7 +1,15 @@
+var previousGrav = null;
+
 AFRAME.registerComponent('negy', {
   init: function () {
     this.el.addEventListener('click', () => {
-      gravity({x: 0, y: -9.81, z: 0})
+      if(previousGrav = "negy"){
+        gravity({x: 0, y: 0, z: 0});
+        previousGrav = null;
+      }else{
+        gravity({x: 0, y: -9.81, z: 0});
+        previousGrav = "negy";
+      }
     })
   }
 });
@@ -9,7 +17,13 @@ AFRAME.registerComponent('negy', {
 AFRAME.registerComponent('posy', {
   init: function () {
     this.el.addEventListener('click', () => {
-      gravity({x: 0, y: 9.81, z: 0})
+      if(previousGrav = "posy"){
+        gravity({x: 0, y: 0, z: 0});
+        previousGrav = null;
+      }else{
+        gravity({x: 0, y: 9.81, z: 0});
+        previousGrav = "posy";
+      }
     })
   }
 });
@@ -17,7 +31,13 @@ AFRAME.registerComponent('posy', {
 AFRAME.registerComponent('posx', {
   init: function () {
     this.el.addEventListener('click', () => {
-      gravity({x: 9.81, y: 0, z: 0})
+      if(previousGrav = "posx"){
+        gravity({x: 0, y: 0, z: 0});
+        previousGrav = null;
+      }else{
+        gravity({x: 9.81, y: 0, z: 0});
+        previousGrav = "posx";
+      }
     })
   }
 });
@@ -25,7 +45,13 @@ AFRAME.registerComponent('posx', {
 AFRAME.registerComponent('negx', {
   init: function () {
     this.el.addEventListener('click', () => {
-      gravity({x: -9.81, y: 0, z: 0})
+      if(previousGrav = "negx"){
+        gravity({x: 0, y: 0, z: 0});
+        previousGrav = null;
+      }else{
+        gravity({x: -9.81, y: 0, z: 0});
+        previousGrav = "negx";
+      }
     })
   }
 });
@@ -33,7 +59,13 @@ AFRAME.registerComponent('negx', {
 AFRAME.registerComponent('posz', {
   init: function () {
     this.el.addEventListener('click', () => {
-      gravity({x: 0, y: 0, z: 9.81})
+      if(previousGrav = "posz"){
+        gravity({x: 0, y: 0, z: 0});
+        previousGrav = null;
+      }else{
+        gravity({x: 0, y: 0, z: 9.81});
+        previousGrav = "posz";
+      }
     })
   }
 });
@@ -41,7 +73,13 @@ AFRAME.registerComponent('posz', {
 AFRAME.registerComponent('negz', {
   init: function () {
     this.el.addEventListener('click', () => {
-      gravity({x: 0, y: 0, z: -9.81})
+      if(previousGrav = "negz"){
+        gravity({x: 0, y: 0, z: 0});
+        previousGrav = null;
+      }else{
+        gravity({x: 0, y: 0, z: -9.81});
+        previousGrav = "negz";
+      }
     })
   }
 });
